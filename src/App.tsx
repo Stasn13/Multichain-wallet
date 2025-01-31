fe8aa94ac21ce7c52c9a29fccbba303ecf68d6b3
Stas <stasn139@gmail.com>
2025-01-31 23:59:15 +0000

    update Backlog

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,9 @@ function App() {
    * 3. Enhance mobile responsiveness
    * 4. Install LI.FI SDK (any objections to use API instead?)
    * 5. Figure out way to connect wallets (wagmi or wallet abstraction through viem or LI.FI SDK?)
+   * 6. Write tests
+   * 7. Write docs
+   * 8. Error and loading states handling
    * 
    */
 
